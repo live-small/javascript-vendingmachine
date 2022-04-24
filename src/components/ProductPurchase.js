16b8ae27c74278d2464c6ce6e72e05304163278c
suhwa <hansuhwa2005@gmail.com>
2022-04-24 21:23:46 +0900

    feat: [상품구매] 투입하기 버튼 클릭시, 자판기에 돈을 투입할 수 있다

--- src/components/ProductPurchase.js
+++ src/components/ProductPurchase.js
@@ -1,10 +1,11 @@
 import Component from "../core/Component.js";
 import ProductPurchaseView from "../template/ProductPurchase.js";
 import { $ } from "../utils/utils.js";
+import { isValidCoinInput } from "../utils/validator.js";
 
 export default class ProductPurchase extends Component {
     template() {
-        return ProductPurchaseView(this.ProductList, 2000, {
+        return ProductPurchaseView(this.ProductList, this.InsertCoin, {
             500: 0,
             100: 4,
             50: 1,
@@ -15,7 +16,10 @@ export default class ProductPurchase extends Component {
     bindEvent() {
         // 투입하기
         $("#charge-button").addEventListener("click", () => {
-            // 저장
+            const inputCoin = $("#charge-input").value;
+            if (isValidCoinInput(inputCoin)) {
+                this.setInsertCoin(Number(inputCoin));
+            }
         });
         // 구매하기
         $(".purchase-button").addEventListener("click", () => {
@@ -25,5 +29,17 @@ export default class ProductPurchase extends Component {
         $("#coin-return-button").addEventListener("click", () => {
             // 남은금액 가져와서, 돈 반환하기
         });
+    }
+
+    get InsertCoin() {
+        if (!localStorage.getItem("insertCoin")) {
+            this.saveLocalStorage("insertCoin", 0);
+        }
+        return JSON.parse(localStorage.getItem("insertCoin"));
+    }
+
+    setInsertCoin(coin) {
+        this.saveLocalStorage("insertCoin", this.InsertCoin + coin);
+        this.render();
     }
 }
