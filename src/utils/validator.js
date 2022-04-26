335f175d8024d87ad320687e907b0117b3fc077a
suhwa <hansuhwa2005@gmail.com>
2022-04-26 11:20:58 +0900

    feat: [잔돈충전] 충전할 돈에 음수값이 올 경우 에러처리 추가

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -4,6 +4,8 @@ const isValidProductPrice = price => price >= 100 && price % 10 === 0;
 
 const isTenUnit = price => price % 10 === 0;
 
+const isNegative = number => number < 0;
+
 export const isValidProduct = ({ name, price, quantity }) => {
     if (isEmptyValue(name)) {
         return alert(`상품명을 입력해주세요`);
@@ -25,6 +27,9 @@ export const isValidProduct = ({ name, price, quantity }) => {
 export const isValidCoinInput = coinToInput => {
     if (isEmptyValue(Number(coinToInput))) {
         return alert(`충전할 돈을 입력해주세요`);
+    }
+    if (isNegative(Number(coinToInput))) {
+        return alert(`음수는 입력할 수 없습니다. 10원 이상부터 충전할 수 있습니다.`);
     }
     if (!isTenUnit(coinToInput)) {
         return alert(`돈은 10원 단위로만 입력할 수 있습니다`);
